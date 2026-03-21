60816ff9355eb5a045163b9ae0523d0faa1a037f
Mátyás Végh <veghmatya@gmail.com>
2026-03-21 22:46:41 +0100

    Added vite

--- dist/scripts/rotating_cube.js
+++
@@ -1,8 +0,0 @@
-import { ScriptComponent } from "../components/script.js";
-import { Time } from "../time.js";
-export class RotatingCube extends ScriptComponent {
-    update() {
-        this.entity.transform.rotation.x = Time.time * 0.3;
-        this.entity.transform.rotation.y = Time.time * 0.8;
-    }
-}
